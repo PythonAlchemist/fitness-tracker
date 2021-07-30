652095f78adad1427676b0d54477e2420bcf21fd
Christopher Singer <christopher.singer.analytics@gmail.com>
2021-07-30 15:05:49 -0400

    setup client

--- Connection.py
+++ Connection.py
@@ -6,6 +6,6 @@ load_dotenv()
 
 client = myfitnesspal.Client(os.getenv('mfpl_user'), password=os.getenv('mfpl_pass'))
 
-day = client.get_date(2021, 7, 30)
+day = client.get_date(2021, 7, 30, username="LindseyStroker")
 
 print(day)
